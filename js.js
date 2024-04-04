3422060af0bb3fbd2ad3430d5b1b859ffb13c906
vrodrig5 <vrodriguez@directvla.com.ar>
2024-04-04 18:35:57 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,42 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, x;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+	x=0
+
+// Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+			x++;
+		}
+    } 
+	
+	document.getElementById("resultado").innerHTML = (9000 - x) + " Dichos encontrados. ";
+}
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
