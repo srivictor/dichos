01689ceccdc283ee900e5e06d782eca0da5fd508
vrodrig5 <vrodriguez@directvla.com.ar>
2024-04-04 20:38:53 -0300

    second commit

--- js.js
+++ js.js
@@ -27,14 +27,14 @@ function myFunction() {
 
 
 // Get the modal
-var modal = document.getElementById('id01');
+//var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+//  if (event.target == modal) {
+//    modal.style.display = "none";
+//  }
+//}
 
 
 
